686631f0b123743fea23baaa73092d448707db84
wjbetech <wjbetech@gmail.com>
2024-08-12 03:01:51 +0900

    Fixing card movement logic /w mouseDown & mouseUp handlers

--- src/components/NoteCard/NoteCard.tsx
+++ src/components/NoteCard/NoteCard.tsx
@@ -24,9 +24,48 @@ export const NoteCard: React.FC<NoteProps> = ({ note }) => {
 
   // setup useState /w useRef for handling x and y coords of cards
   const [position, setPosition] = useState(note.position);
-
   const initialMousePos = { x: 0, y: 0 };
-  const cardPositionRef = useRef(null);
+  const cardPositionRef = useRef<HTMLDivElement>(null);
+
+  // capture mouse down event and initiate movement
+  const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (cardPositionRef.current) {
+      initialMousePos.x = e.clientX;
+      initialMousePos.y = e.clientY;
+      document.addEventListener('mousemove', handleMouseMove);
+      document.addEventListener('mouseup', handleMouseUp);
+    }
+  };
+
+  // capture all mouse movement events and update card position
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    // debugging
+    if (!cardPositionRef.current) {
+      return;
+    }
+
+    // handle move direction
+    const moveDirection = {
+      x: initialMousePos.x - e.clientX,
+      y: initialMousePos.y - e.clientY
+    };
+
+    // update start position for next move
+    initialMousePos.x = e.clientX;
+    initialMousePos.y = e.clientY;
+
+    // update card position
+    setPosition({
+      x: cardPositionRef.current.offsetLeft - moveDirection.x,
+      y: cardPositionRef.current.offsetTop - moveDirection.y
+    });
+  };
+
+  // stop movement on mouse up
+  const handleMouseUp = () => {
+    document.removeEventListener('mousemove', handleMouseMove);
+    document.removeEventListener('mouseup', handleMouseUp);
+  };
 
   // setup useRef /w useEffect for textArea auto adjust
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
@@ -47,11 +86,15 @@ export const NoteCard: React.FC<NoteProps> = ({ note }) => {
     // card
     <div
       ref={cardPositionRef}
-      className="absolute w-[400px] cursor-pointer shadow-md"
+      className="absolute w-[400px] cursor-pointer shadow-md space-y-2"
       style={{ backgroundColor: colors.colorBody, left: `${note.position.x}`, top: `${note.position.y}` }}
     >
       {/* card header */}
-      <div className="border-r-4 flex justify-between align-middle p-2" style={{ backgroundColor: colors.colorHeader }}>
+      <div
+        onMouseDown={handleMouseDown}
+        className="border-r-4 flex justify-between align-middle p-2"
+        style={{ backgroundColor: colors.colorHeader }}
+      >
         <div>
           <Trash />
         </div>
